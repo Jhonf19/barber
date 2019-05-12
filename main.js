b93e2575b1b277d00f286a06ccfdbffe68fa11af
jhonf19 <zjhon2018@gmail.com>
2019-05-11 20:04:36 -0500

    new branch

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 $(function() {
+
   $(".menu-icon").on("click", function() {
     $("nav ul").toggleClass("showing");
   });
+
 });
+
+
 $(window).on("scroll", function() {
   if ($(window).scrollTop()) {
     $("nav").addClass("black");
+    $("h1").addClass("h");
   }else {
     $("nav").removeClass("black");
+    $("h1").removeClass("h");
   }
 });
