6384679a9bdeced38c5db625b93610d4f348237b
jhonf19 <zjhon2018@gmail.com>
2019-05-13 13:56:25 -0500

    my nav 3

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-const navSlide = ()=>{
-    const icon = document.querySelector('.menu-icon');
-    const nav = document.querySelector('.nav-link');
-    icon.addEventListener('click', ()=>{
-      nav.classList.toggle('nav-active')
-    })
-}
-navSlide()
+$(()=> {
+  $(".btn-toggle i").on("click", ()=>{
+    $("nav").slideToggle()
+  })
+})
