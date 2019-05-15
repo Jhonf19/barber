0c7a166ba9d9503d817671030dcfe02ea6456d86
jhonf19 <zjhon2018@gmail.com>
2019-05-14 19:41:53 -0500

    my nav 5

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 $(()=> {
-  $(".btn-menu").on("click", ()=>{
-    $(".btn-menu").toggleClass("btn-on-menu")
-    $(".menu").slideToggle()
+  $(".menu-icon").on("click", ()=>{
+    $("header nav ul").slideToggle();
+    // $(".btn-menu").toggleClass("btn-on-menu")
+    // $(".menu").slideToggle()
     // $(".btn-menu").toggleClass("btn btn-dark")
     // alert("WW")
   })
