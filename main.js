bab6ee0b04e0faab731d19320314bfd3bf5c4d2e
jhonf19 <zjhon2018@gmail.com>
2019-05-16 22:10:01 -0500

    landing page responsive ok

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-$(function() {
-  $(".menu-icon").on("click", function() {
-    $("nav ul").toggleClass("showing");
-  });
-});
-$(window).on("scroll", function() {
-  if ($(window).scrollTop()) {
-    $("nav").addClass("black");
-  }else {
-    $("nav").removeClass("black");
-  }
-});
+$(()=> {
+  $(".menu-icon").on("click", ()=>{
+    $("header nav ul").slideToggle();
+    // $(".btn-menu").toggleClass("btn-on-menu")
+    // $(".menu").slideToggle()
+    // $(".btn-menu").toggleClass("btn btn-dark")
+    // alert("WW")
+  })
+  $(window).scroll(()=>{
+
+    console.log($("#about").scrollTop());
+  })
+})
